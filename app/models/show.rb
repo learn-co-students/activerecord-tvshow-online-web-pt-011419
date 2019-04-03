7712a9cbdd4190622b02718c64bb894aeab12b7d
dclements9 <dclements@protonmail.com>
2019-04-03 09:18:00 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,31 +3,31 @@ require 'pry'
 class Show < ActiveRecord::Base
     
     def highest_rating
-        Show.maximum(:ratings)
+        self.maximum(:rating)
     end
 
     def most_popular_show
-        Show.highest_rating
+        self.highest_rating.first
         binding.pry
     end
 
     def lowest_rating
-
+        self.minimum(:rating)
     end
 
     def least_popular_show
-
+        self.lowest_rating.first
     end
 
     def ratings_sum
-        Show.sum(:ratings)
+        self.sum(:rating)
     end
 
     def popular_shows
-
+        self.where(":rating > ?", 5)
     end
 
     def shows_by_alphabetical_order
-
+        self.order(:name)
     end
 end
