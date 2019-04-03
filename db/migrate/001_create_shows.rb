c155b7c1ac5f93aa048ae8181e2be85327460025
dylanclements <dclements@protonmail.com>
2019-04-03 12:43:07 -0400

    Done.

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,10 @@
 class CreateShows < ActiveRecord::Migration
     def change
         create_table :shows do |column|
-            column.string name
-            column.string network
-            column.string day
-            column.integer rating
+            column.string :name
+            column.string :network
+            column.string :day
+            column.integer :rating
         end
     end
 end
